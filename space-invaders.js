e47de7f2d59022ac9972e366c251a49d6755f94f
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-02 16:23:17 +0000

    added bullet

--- space-invaders.js
+++ space-invaders.js
@@ -5,9 +5,12 @@ let x = canvas.width / 2;
 let y = canvas.height - 200;
 let leftPressed = false;
 let rightPressed = false;
-let enemyx = 100;
-let enemyy = 100;
-let enemyMove = true;
+let spacePressed = false;
+let enemyX = 100;
+let enemyY = 100;
+let bulletX = x + 50;
+let bulletY = y - 25;
+let enemyMove = false;
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -18,28 +21,32 @@ function draw() {
   if (rightPressed === true && x + 5 < 1475) {
     x += 25;
   }
-  if (enemyMove === true) {
-    enemyx += 30;
+  if (enemyMove === false) {
+    enemyX += 30;
   } else {
-    enemyx -= 30;
+    enemyX -= 30;
   }
-  if (enemyx >= 1500) {
-    enemyMove = false;
-    enemyy += 50;
-  }
-  if (enemyx <= 0) {
+  if (enemyX >= 1500) {
     enemyMove = true;
-    enemyy += 50;
+    enemyY += 50;
   }
- 
-
+  if (enemyX <= 0) {
+    enemyMove = false;
+    enemyY += 50;
+  }
+  
+  
   drawShip();
   drawEnemy();
+  if (spacePressed === true) {
+    drawBullet();
+    bulletY -= 15;
+  }
 }
 
 function drawEnemy() {
   ctx.beginPath();
-  ctx.rect(enemyx, enemyy, 100, 100);
+  ctx.rect(enemyX, enemyY, 100, 100);
   ctx.stroke();
   ctx.fill();
   ctx.closePath();
@@ -51,106 +58,35 @@ function drawShip() {
   ctx.fill();
   ctx.closePath();
 }
-//  add enemy
+
+function drawBullet() {
+  ctx.beginPath();
+  ctx.rect(bulletX, bulletY, 25, 25);
+  ctx.stroke();
+  ctx.fill();
+  ctx.closePath();
+}
 setInterval(draw, 100);
 
 function handleKeyDown(event) {
-  console.log(event);
   if (event.keyCode === 37) {
     leftPressed = true;
   } else if (event.keyCode === 39) {
     rightPressed = true;
+  } else if (event.keyCode === 32) {
+    spacePressed = true;
+    console.log(`Space Pressed`);
   }
 }
 
 function handleKeyUp(event) {
-  console.log(event);
   if (event.keyCode === 37) {
     leftPressed = false;
   } else if (event.keyCode === 39) {
     rightPressed = false;
+  
   }
 }
 
-// function moveShip(event) {
-//   console.log(event.keyCode);
-//   switch (event.keyCode) {
-//     case 37:
-//       console.log(`left arrow pressed`);
-//       x -= 5;
-//       break;
-//     case 39:
-//       console.log(`right arrow pressed`);
-//       x += 5;
-//       break;
-//   }
-// }
-
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
-
-// Step 1
-// Add two variables - leftPressed and rightPressed
-// Add keydown and keyup event listeners
-// Add functions that are called
-//
-
-// const MOVE_AMOUNT = 10;
-
-// const { width, height } = canvas;
-
-// let x = Math.floor(Math.random() * width);
-// let y = Math.floor(Math.random() * height);
-
-// function draw({ key }) {
-//   hue = hue + 10;
-//   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-//   console.log(key);
-//   ctx.beginPath();
-//   ctx.moveTo(x, y);
-//   switch (key) {
-//     case "ArrowUp":
-//       y -= MOVE_AMOUNT;
-//       break;
-//     case "ArrowRight":
-//       x += MOVE_AMOUNT;
-//       break;
-//     case "ArrowDown":
-//       y += MOVE_AMOUNT;
-//       break;
-//     case "ArrowLeft":
-//       x -= MOVE_AMOUNT;
-//       break;
-//     default:
-//       break;
-
-//   }
-//   ctx.lineTo(x, y);
-//   ctx.stroke();
-// }
-
-// function handleKey(e) {
-//   if (e.key.includes("Arrow")) {
-//     e.preventDefault();
-//     draw({ key: e.key });
-//   }
-// }
-
-// function makeSquare() {
-//   // x and y have a value at this point
-//   // you can update x and y
-
-//   ctx.beginPath();
-//   ctx.moveTo(x, y);
-//   x += 150;
-//   ctx.lineTo(x, y);
-//   y += 150;
-//   ctx.lineTo(x, y);
-//   x -= 150;
-//   ctx.lineTo(x, y);
-//   ctx.closePath();
-//   y -= 150;
-//   ctx.stroke();
-// }
-
-// window.addEventListener("keydown", handleKey);
