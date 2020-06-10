faaf8349b54d2364c9691c1c13c51a2ad5530262
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-06-10 21:08:00 +0100

    scratchs 8, movement

--- scratchs/scratch8.js
+++ scratchs/scratch8.js
@@ -3,38 +3,39 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 200;
 let enemies = [];
-let enemyMove = false;
 
 function initEmemy() {
   for (let j = 0; j < 5; j++) {
-    enemies.push({ x: 100 + 200 * j, y: 100 });
+    enemies.push({ x: 100 + 200 * j, y: 100, direction: 1 });
   }
 }
 initEmemy();
 
-function draw() {
+function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let j = 0; j < 5; j++) {
+}
+
+function drawEnemy() {
+  for (let i = 0; i < 5; i++) {
     ctx.beginPath();
-    ctx.rect(enemies[j].x, enemies[j].y, 100, 100);
+    ctx.rect(enemies[i].x, enemies[i].y, 100, 100);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
-    enemies[j].x += 10;
-    
-    if (enemyMove === false) {
-      enemies[j].x += 25;
-    } else {
-      enemies[j].x -= 25;
-    }
-    if (enemies[j].x >= 1500) {
-      enemyMove = true;
-      enemies[j].y += 50;
-    }
-    if (enemies[j].x <= 0) {
-      enemyMove = false;
-      enemies[j].y += 50;
-    }
   }
 }
+
+function enemyMove() {
+  for (let enemy of enemies) {
+    enemy = enemies[i].x;
+    enemy += 10;
+  }
+}
+
+function draw() {
+  clearCanvas();
+  drawEnemy();
+  enemyMove();
+}
+
 setInterval(draw, 50);
