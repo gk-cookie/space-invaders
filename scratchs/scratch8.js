b37a80c84ee40e84721320b6c67f8372842871ff
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-06-10 13:22:47 +0100

    updated enemies

--- scratchs/scratch8.js
+++ scratchs/scratch8.js
@@ -2,23 +2,35 @@ const canvas = document.querySelector("#space-invaders");
 const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 200;
-enemy = [];
+let enemies = [];
+let enemyMove = false;
 
-ctx.beginPath();
-ctx.rect(x, y, 100, 100);
-ctx.stroke();
-ctx.fill();
-ctx.closePath();
-console.log(enemy);
+// ctx.beginPath();
+// ctx.rect(x, y, 100, 100);
+// ctx.stroke();
+// ctx.fill();
+// ctx.closePath();
+// console.log(enemy);
 
 function initEmemy() {
-    for (let j = 0; j > 5; j++) {
-        enemy.push({ x: 100, y: 100 });
-        ctx.beginPath();
-    ctx.rect(enemy[j].x, enemy[j].y, 100, 100);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
+  for (let j = 0; j < 5; j++) {
+    enemies.push({ x: 100 + 200 * j, y: 100 });
   }
 }
 initEmemy();
+// console.log(enemies);
+// console.log(enemies.y);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let j = 0; j < 5; j++) {
+    ctx.beginPath();
+    ctx.rect(enemies[j].x, enemies[j].y, 100, 100);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+    enemies[j].x += 10;
+    initEmemy();
+  }
+}
+setInterval(draw, 50);
