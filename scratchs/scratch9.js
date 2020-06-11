ec878c378efe2ef262e4b47e07c96a2b63ffff61
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-06-11 11:53:23 +0100

    spaceship on screen

--- scratchs/scratch9.js
+++ scratchs/scratch9.js
@@ -3,3 +3,20 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 200;
 
+function spaceship() {
+    ctx.beginPath();
+    ctx.rect(x, y, 100, 100);
+    ctx.fill();
+    ctx.closePath();
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+function draw() {
+clearCanvas();
+spaceship()
+}
+
+setInterval(draw, 50);
